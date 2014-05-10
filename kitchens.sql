1d8f5ee2abd142cec860e0a15c72c2b280126648
Animesh Ramesh <animeshramesh@gmail.com>
2014-05-11 03:11:51 +0530

    Added server support for kitchens

---
+++ kitchens.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Kitchens` (
+  `Slno` varchar(100) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `latitude` varchar(100) NOT NULL,
+  `longitude` varchar(100) NOT NULL
+);
+
+
+insert into 'Kitchens'(Slno, Name, latitude,longitude) values("1", "hkh","13.011162","77.550695");
+insert into 'Kitchens'(Slno, Name, latitude,longitude) values("2", "vkh","13.017434","77.549751");
+
+	
