cedb3c15cfd76f6d7ee8dbdc963a403c2532c1c2
sudheesh001 <sudheesh1995@outlook.com>
2014-05-10 22:13:42 +0530

    Enabling CORS support

--- server.py
+++ server.py
@@ -5,6 +5,47 @@ import sqlite3
 import json, requests
 app = Flask(__name__)
 
+
+@app.before_request
+def option_autoreply():
+    """ Always reply 200 on OPTIONS request """
+
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
+
+@app.after_request
+def set_allow_origin(resp):
+    """ Set origin for GET, POST, PUT, DELETE requests """
+
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if request.method != 'OPTIONS' and 'Origin' in request.headers:
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+
+    return resp
+
 @app.route('/')
 def parse_request():
     route_id = request.args.get('r', None)
@@ -23,6 +64,7 @@ def return_response(route_id):
     routes = []
     for each_row in rows:
         location = {}
+        location['Name'] = each_row[1]
         location['latitude'] = each_row[3]
         location['longitude'] = each_row[4]
         routes.append(location)
