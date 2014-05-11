2e39ae88cb357c2c2890152ebe0930f7ecd2e9a9
sudheesh001 <sudheesh1995@outlook.com>
2014-05-11 07:36:17 +0530

    DEMO level hack

--- server.py
+++ server.py
@@ -7,6 +7,46 @@ import urllib2
 import json, requests
 app = Flask(__name__)
 
+@app.before_request
+def option_autoreply():
+    """ Always reply 200 on OPTIONS request """
+
+    if request.method == 'OPTIONS':
+        resp = app.make_default_options_response()
+
+        headers = None
+        if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+            headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+        h = resp.headers
+
+        # Allow the origin which made the XHR
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+        # Allow the actual method
+        h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+        # Allow for 10 seconds
+        h['Access-Control-Max-Age'] = "10"
+
+        # We also keep current headers
+        if headers is not None:
+            h['Access-Control-Allow-Headers'] = headers
+
+        return resp
+
+
+@app.after_request
+def set_allow_origin(resp):
+    """ Set origin for GET, POST, PUT, DELETE requests """
+
+    h = resp.headers
+
+    # Allow crossdomain for other HTTP Verbs
+    if request.method != 'OPTIONS' and 'Origin' in request.headers:
+        h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+
+    return resp
+
 @app.route('/')
 def parse_request():
     route_id = request.args.get('r', None)
@@ -18,7 +58,6 @@ def parse_request():
 
 def return_response(route_id, kitchen_id):
     conn = sqlite3.connect('data.db')
-    conn2 = sqlite3.connect()
     c = conn.cursor()
 
 
@@ -43,6 +82,9 @@ def return_response(route_id, kitchen_id):
 
     conn2 = sqlite3.connect('kitchens.db')
     c2 = conn2.cursor()
+    
+
+
     command = ("SELECT * FROM Kitchens WHERE Name = '%s'" % kitchen_id)
     row = c2.execute(command).fetchall()
     #print row
